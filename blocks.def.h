b079c4ef85886bc05c71c8cc3644006c86a5866a
Dan Snow <danjsnow@outlook.com>
2021-04-15 20:17:39 +0100

    Added some more bash scripts to status

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "~/dotfiles/status_scripts/watson.sh",					      60,		6},
+	{"", "~/dotfiles/status_scripts/email_status.sh",					60,		1},
+	{"", "~/dotfiles/status_scripts/wifi.sh",					        60,		2},
+	{"", "~/dotfiles/status_scripts/volume.sh",					      60,		3},
+	{"", "~/dotfiles/status_scripts/battery.sh",					    60,		4},
+	{"", "~/dotfiles/status_scripts/calcurse.sh",					    60,		7},
+	{"", "~/dotfiles/status_scripts/date.sh",					        15,		5},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
