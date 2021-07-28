035a6c9fb1f5bef0787897034a8cb97dac0201db
Dan Snow <danjsnow@outlook.com>
2021-07-28 18:17:39 +0100

    Changed order + removed divider which is added by script now

--- blocks.def.h
+++ blocks.def.h
@@ -1,16 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "~/dotfiles/status_scripts/watson.sh",					      60,		6},
-	{"", "~/dotfiles/status_scripts/email_status.sh",					60,		1},
-	{"", "~/dotfiles/status_scripts/wifi.sh",					        60,		2},
+	{"", "~/dotfiles/status_scripts/timetrace.sh",					  60,		6},
 	{"", "~/dotfiles/status_scripts/volume.sh",					      60,		3},
-	{"", "~/dotfiles/status_scripts/bluetooth.sh",					  60,		8},
 	{"", "~/dotfiles/status_scripts/battery.sh",					    60,		4},
 	{"", "~/dotfiles/status_scripts/calcurse.sh",					    60,		7},
+	{"", "~/dotfiles/status_scripts/wifi.sh",					        60,		2},
+	{"", "~/dotfiles/status_scripts/email_status.sh",					60,		1},
 	{"", "~/dotfiles/status_scripts/date.sh",					        15,		5},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = " ";
 static unsigned int delimLen = 5;
