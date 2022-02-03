544db7f4b620ccab6f55b89136d711e7b9efb034
Dan Snow <danjsnow@outlook.com>
2022-02-03 14:09:13 +0000

    Changed out email blocks

--- blocks.def.h
+++ blocks.def.h
@@ -2,7 +2,8 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "~/dotfiles/status_scripts/timetrace.sh",					  60,		6},
-	{"", "~/dotfiles/status_scripts/email_status.sh",					60,		1},
+//{"", "~/dotfiles/status_scripts/email_status.sh",					60,		1},
+  {"", "~/dotfiles/status_scripts/timetrace_hours.sh",		  60,		1},
 	{"", "~/dotfiles/status_scripts/volume.sh",					      60,		3},
 	{"", "~/dotfiles/status_scripts/wifi.sh",					        60,		2},
 	{"", "~/dotfiles/status_scripts/battery.sh",					    60,		4},
