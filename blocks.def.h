37d7c9ddef6341afea3aa49d56c8e1af29ddd1a1
Dan Snow <danjsnow@outlook.com>
2022-08-17 16:19:30 +0100

    Added programming timer

--- blocks.def.h
+++ blocks.def.h
@@ -1,14 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "~/dotfiles/status_scripts/timetrace.sh",					  60,		6},
-  {"", "~/dotfiles/status_scripts/timetrace_hours.sh",		  60,		1},
-  {"", "~/dotfiles/status_scripts/email_status.sh",		      60,		8},
-	{"", "~/dotfiles/status_scripts/volume.sh",					      60,		3},
-	{"", "~/dotfiles/status_scripts/wifi.sh",					        60,		2},
-	{"", "~/dotfiles/status_scripts/battery.sh",					    60,		4},
-	{"", "~/dotfiles/status_scripts/khal_week.sh",					  600,	7},
-	{"", "~/dotfiles/status_scripts/date.sh",					        15,		5},
+	{"", "~/dotfiles/status_scripts/task_status.sh",					   60,		10},
+  {"", "~/dotfiles/status_scripts/program_work_hour_track.sh", 60,		9},
+  {"", "~/dotfiles/status_scripts/deep_work_hour_track.sh",		 60,		8},
+  {"", "~/dotfiles/status_scripts/shallow_work_hour_track.sh", 60,		7},
+  {"", "~/dotfiles/status_scripts/email_status.sh",		         60,		6},
+	{"", "~/dotfiles/status_scripts/volume.sh",					         60,		5},
+	{"", "~/dotfiles/status_scripts/wifi.sh",					           60,		4},
+	{"", "~/dotfiles/status_scripts/battery.sh",					       60,		3},
+	{"", "~/dotfiles/status_scripts/khal_week.sh",					     600,	  2},
+	{"", "~/dotfiles/status_scripts/date.sh",					           15,		1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
